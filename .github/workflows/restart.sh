2557e22e558f9566383cb9f9ed6090df811859fc
Vicki Boykis <vicki.boykis@gmail.com>
2024-12-14 10:53:45 -0500

    with echo

--- .github/workflows/restart.sh
+++ .github/workflows/restart.sh
@@ -1,6 +1,14 @@
 cd /var/www/html
-chmod +x ingest serve
-killall -0 ingest 2>/dev/null && pkill -f 'ingest'
-killall -0 serve 2>/dev/null && pkill -f 'serve'
+echo "Changed directory to /var/www/html"
+
+pkill -f 'ingest'
+echo "Killed ingest processes"
+
+pkill -f 'serve'
+echo "Killed serve processes"
+
 nohup ./ingest > ingest.log 2>&1 &
-nohup ./serve > serve.log 2>&1 & 
+echo "Started ingest process"
+
+nohup ./serve > serve.log 2>&1 &
+echo "Started serve process"

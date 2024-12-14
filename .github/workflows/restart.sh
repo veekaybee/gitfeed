523636f04c2d3b6612bee555666fdeca288f0e56
Vicki Boykis <vicki.boykis@gmail.com>
2024-12-14 10:48:07 -0500

    chmod fix

--- .github/workflows/restart.sh
+++ .github/workflows/restart.sh
@@ -1,5 +1,6 @@
 cd /var/www/html
-pkill -f 'ingest' || true
-pkill -f 'serve' || true
+chmod +x ingest serve
+killall -0 ingest 2>/dev/null && pkill -f 'ingest'
+killall -0 serve 2>/dev/null && pkill -f 'serve'
 nohup ./ingest > ingest.log 2>&1 &
 nohup ./serve > serve.log 2>&1 & 

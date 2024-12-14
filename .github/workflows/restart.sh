df116a861a92896f307bfa64cf6df21fb075702d
Vicki Boykis <vicki.boykis@gmail.com>
2024-12-14 10:28:00 -0500

    reformat shell

--- .github/workflows/restart.sh
+++ .github/workflows/restart.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Change to the application directory
 cd /var/www/html
 
-# Kill existing processes (suppress errors if processes don't exist)
+
 pkill -f 'ingest' || true
 pkill -f 'serve' || true
 
-# Start services in background and redirect output to log files
+
 nohup ./ingest > ingest.log 2>&1 &
 nohup ./serve > serve.log 2>&1 &

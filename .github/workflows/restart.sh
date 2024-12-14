c3f1863cf920a109c51aca286e799753dd4acc91
Vicki Boykis <vicki.boykis@gmail.com>
2024-12-14 10:34:20 -0500

    reformat shell

--- .github/workflows/restart.sh
+++ .github/workflows/restart.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-
-cd /var/www/html
-
-
-pkill -f 'ingest' || true
-pkill -f 'serve' || true
-
-
-nohup ./ingest > ingest.log 2>&1 &
-nohup ./serve > serve.log 2>&1 &
+cd /var/www/html && \
+pkill -f 'ingest' || true && \
+pkill -f 'serve' || true && \
+nohup ./ingest > ingest.log 2>&1 & \
+nohup ./serve > serve.log 2>&1 & 

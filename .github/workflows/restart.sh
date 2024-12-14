d571d4b11079d33b899bcea04f35463281380a77
Vicki Boykis <vicki.boykis@gmail.com>
2024-12-14 11:00:25 -0500

    wait for kill process

--- .github/workflows/restart.sh
+++ .github/workflows/restart.sh
@@ -1,11 +1,16 @@
 cd /var/www/html
 echo "Changed directory to /var/www/html"
 
-pkill -f 'ingest'
+# Only kill if processes exist
+pgrep -f 'ingest' && pkill -f 'ingest'
 echo "Killed ingest processes"
 
-pkill -f 'serve'
+pgrep -f 'serve' && pkill -f 'serve'
 echo "Killed serve processes"
+
+# Wait a moment for processes to clean up
+sleep 2
+echo "Waited for cleanup"
 
 nohup ./ingest > ingest.log 2>&1 &
 echo "Started ingest process"
